4c7a5cea635f015547b39804d5621abbcd1a9482
Jibril1010 <Jw7677@nyu.edu>
2025-04-07 13:47:01 -0400

    formatting

--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,14 @@
 """Flask app to render the index page."""
 from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route('/')
+
+
+@app.route("/")
 def index():
     """Render the homepage."""
-    return render_template('index.html')
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
