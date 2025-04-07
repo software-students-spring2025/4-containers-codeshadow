586662c620a3cf25ae98ebac92ecadac4191ff10
Jibril1010 <Jw7677@nyu.edu>
2025-04-07 13:23:09 -0400

    pipfile update

--- web-app/app.py
+++ web-app/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template 
+"""Flask app to render the index page."""
 
+from flask import Flask, render_template
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-
 def index():
+    """Render the homepage."""
     return render_template('index.html')
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
