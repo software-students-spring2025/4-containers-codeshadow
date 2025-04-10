e002f11c9f843d693056c62bd92ab00dc1c1ce4e
oadesiyan <oluwapelumiadesiyan04@gmail.com>
2025-04-10 12:47:45 -0400

    added tracker page & slight cleanup

--- web-app/app.py
+++ web-app/app.py
@@ -1,13 +1,6 @@
 """Main Flask application for the emotion detection web app."""
-
-import os
-import sys
-from datetime import datetime
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
-
 import flask
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 import flask_login
 from flask_login import current_user, login_required
 import pymongo
@@ -16,13 +9,14 @@ import os
 import sys
 import certifi
 from bson.objectid import ObjectId
-
+from datetime import datetime
+from flask_cors import CORS
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "devkey")
-
 
 #login 
 login_manager = flask_login.LoginManager()
@@ -34,20 +28,15 @@ client = pymongo.MongoClient(os.getenv("MONGO_URI"), tlsCAFile=certifi.where())
 db = client[os.getenv("MONGO_DBNAME")]
 users = db.users
 emotions= db.Emotions 
-print( )
-# Get the absolute path to the parent directory
+
+#path to the parent directory
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# Add the path to the machine-learning-client directory
+
+#path to the machine-learning-client directory
 sys.path.append(os.path.join(parent_dir, "machine-learning-client"))
 from ai import detect_emotion
 
 last_emotion = {"emotion": None, "start_time": datetime.utcnow()}
-
-#app = Flask(
-#    __name__,
-#    template_folder="templates",
-#    static_folder="static"
-#)
 
 CORS(app)
 
@@ -88,31 +77,9 @@ def login():
         '''
     #return render_template("templates/login.html")
 
-@app.route("/index")
-def index():
-    """Render the homepage."""
-    print("CURRENT USER:", current_user.username)
-    user_doc = users.find_one({"username": current_user.username})
-    if not user_doc:
-        flash("User not found", "danger")
-        return redirect(url_for("login"))
-
-    # Get the most recent emotion detected
-    current_emotion = last_emotion["emotion"]
-
-    # Get the emoji for that emotion
-    #emotion_doc = emotions.find_one({"Name": current_user.username})
-    emotion_doc = emotions.find_one({"Name": current_user.username})
-    emoji = emotion_doc.get(current_emotion, "🤔") if emotion_doc and current_emotion else "🤔"
-
-
-    return render_template("index.html", emoji=emoji)
-
-
-
-
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    """Create an account"""
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -148,51 +115,24 @@ def signup():
 
     return render_template("signup.html")
 
-class User(flask_login.UserMixin):
-    def __init__(self, user_doc):
-        self.id = str(user_doc["_id"])
-        self.username = user_doc["username"]
-        self.password = user_doc["password"]
 
-    @staticmethod
-    def is_authenticated():
-        return True
-
-    @staticmethod
-    def is_active():
-        return True
-
-    @staticmethod
-    def is_anonymous():
-        return False
-
-    def get_id(self):
-        return self.id
-
-
-@login_manager.user_loader
-def user_loader(user_id):
-    user_doc = users.find_one({"_id": ObjectId(user_id)})
+@app.route("/index")
+def index():
+    """Render the homepage."""
+    print("CURRENT USER:", current_user.username)
+    user_doc = users.find_one({"username": current_user.username})
     if not user_doc:
-        return
-    
-    return User(user_doc)
+        flash("User not found", "danger")
+        return redirect(url_for("login"))
 
+    # Get the most recent emotion detected
+    current_emotion = last_emotion["emotion"]
 
-@login_manager.request_loader
-def request_loader(request):
-    username = request.form.get('username')
+    # Get the emoji for that emotion
+    emotion_doc = emotions.find_one({"Name": current_user.username})
+    emoji = emotion_doc.get(current_emotion, "🤔") if emotion_doc and current_emotion else "🤔"
 
-    if not username:
-        return
-    
-    user_doc = users.find_one({"username": username})
-
-    if not user_doc: #user doesn't exist
-        return None
-
-    return User(user_doc)
-
+    return render_template("index.html", emoji=emoji)
 
 @app.route("/submit-image", methods=["POST"])
 def submit_image():
@@ -229,9 +169,31 @@ def submit_image():
      return jsonify({"emotion": "unknown", "emoji": "🤔"})
     #########
 
+@app.route("/track")
+def track():
+    """Render the emotion tracker page for the logged-in user."""
+    # Get the user data from the database
+    emotion_doc = emotions.find_one({"Name": current_user.username})
+
+    if not emotion_doc:
+        flash("Emotion data not found for the user.", "danger")
+        return redirect(url_for("index"))
+
+    # Prepare the data to display on the tracker page
+    emotion_summary = {
+        "anger": emotion_doc.get("anger_count", 0),
+        "disgust": emotion_doc.get("disgust_count", 0),
+        "fear": emotion_doc.get("fear_count", 0),
+        "happy": emotion_doc.get("happy_count", 0),
+        "neutral": emotion_doc.get("neutral_count", 0),
+        "sad": emotion_doc.get("sad_count", 0),
+        "surprise": emotion_doc.get("surprise_count", 0)
+    }
+    
+    # Render the tracker page and pass the emotion summary data
+    return render_template("tracker.html", emotion_summary=emotion_summary)
 
 
-#####
 DEFAULT_EMOTION_DATA = {
     "anger": "😡",
     "disgust": "🤢",
@@ -257,8 +219,48 @@ def ensure_emotion_data_for_user(username):
             **DEFAULT_EMOTION_DATA
         })
 
+class User(flask_login.UserMixin):
+    def __init__(self, user_doc):
+        self.id = str(user_doc["_id"])
+        self.username = user_doc["username"]
+        self.password = user_doc["password"]
 
+    @staticmethod
+    def is_authenticated():
+        return True
 
-####
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.id
+
+@login_manager.user_loader
+def user_loader(user_id):
+    user_doc = users.find_one({"_id": ObjectId(user_id)})
+    if not user_doc:
+        return
+    
+    return User(user_doc)
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+
+    if not username:
+        return
+    
+    user_doc = users.find_one({"username": username})
+
+    if not user_doc: #user doesn't exist
+        return None
+
+    return User(user_doc)
+
 if __name__ == "__main__":
     app.run(debug=True)
